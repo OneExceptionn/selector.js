0da29c7c1eb22d0d96f271321c6395d3af3ab5c5
Lawrence <lkuang93@gmail.com>
2020-07-16 21:05:47 -0400

    updated js

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,55 @@
 // YOUR CODE HERE
+// function $(cssSelector) {
+//     this.elements = [];
+//     if (cssSelector[0] === '.') {
+//         let classArray = Array.from(document.getElementsByClassName(cssSelector.slice(1)));
+//         classArray.forEach(element => this.elements.push(element))
+//     } else if (cssSelector[0] === '#') {
+//         let idElement = (document.getElementById(cssSelector.slice(1)));
+//         this.elements.push(idElement);
+//     } else {
+//         let tagArray = Array.from(document.getElementsByTagName(cssSelector));
+//         tagArray.forEach(element => this.elements.push(element))
+//     }
+// }
+
+class $ {
+    constructor (cssSelector) {
+        this.elements = []
+        if (cssSelector[0] === '.') {
+            let classArray = Array.from(document.getElementsByClassName(cssSelector.slice(1)));
+            classArray.forEach(element => this.elements.push(element))
+        } else if (cssSelector[0] === '#') {
+            let idElement = (document.getElementById(cssSelector.slice(1)));
+            this.elements.push(idElement);
+        } else {
+            let tagArray = Array.from(document.getElementsByTagName(cssSelector));
+            tagArray.forEach(element => this.elements.push(element))
+        }
+    }
+
+    hide () {
+        this.elements.forEach(element => element.style.display = 'none')
+    }
+
+    show () {
+        this.elements.forEach(element => element.style.display = 'inherit')
+    }
+
+    addClassName (name) {
+        this.elements.forEach(element => element.classList = name)
+    }
+
+    removeClassName (name) {
+        this.elements.forEach(element => element.classList.remove(name))
+    }
+
+    text(str) {
+        this.elements.forEach(element => element.innerText = str)
+    }
+
+    addChild(tag) {
+        this.elements.forEach(element => element.innerHTML = `<${tag}></${tag}>`)
+    }
+
+}
